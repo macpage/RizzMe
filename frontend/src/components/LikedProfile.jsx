c17e476f0507d02b3c3f174a2c6d13f01388986a
Mac <sophanpa@gmail.com>
2023-09-28 21:10:36 +0200

    Edit

--- frontend/src/components/LikedProfile.jsx
+++ frontend/src/components/LikedProfile.jsx
@@ -1,6 +1,23 @@
-function LikedProfile(prop){
+import axios from "axios";
+import { useState } from "react"
+
+ function LikedProfile(prop){
+    const [pics,setPics] = useState([null]);
+    let pic = "http://localhost:3004/images/";
+    async function getPic(){
+          if(pics[0] == null){
+         await axios.get("http://localhost:3004/getImage").then(data => setPics(data.data)).catch(err=>console.log(err))
+    }  
+    pics.forEach(e => {
+       if(e.username == prop.username && e.index == 0){
+        pic += e.image;
+        console.log(pic);
+       }
+    });
+    }
+getPic();
     return <div id="LikedProfile">
-        <img src="" alt="" />
+        <img src={pic} alt="" />
         <h1>{prop.username}</h1>
         <p></p>
         </div>
--- frontend/src/pages/LikePage.jsx
+++ frontend/src/pages/LikePage.jsx
@@ -5,13 +5,13 @@ import LikedProfile from "../components/LikedProfile";
 function LikePage(prop){
 
 const [likes,setLikes] = useState([null]);
+const [imgs,setImgs] = useState([null]);
 
 async function getLikes(){
-    if(likes[0] == null){
-   await axios.get("http://localhost:3004/updateLiked", {params:{username: prop.username}}).then(data => setLikes(data.data.liked)).catch(err=>console.log(err))
-
-    }
+  if(likes[0] == null){
+   await axios.get("http://localhost:3004/updateLiked", {params:{username: prop.username}}).then(data => {setLikes(data.data.liked)} ).catch(err=>console.log(err))
   
+    }
 }
 getLikes();
     return <div id="LikePage">
