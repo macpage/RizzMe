2ebecaea3509ab2e3946e5bd6eab4c2d2482b90c
Mac <sophanpa@gmail.com>
2023-10-02 22:41:21 +0200

    Edit

--- frontend/src/pages/LikePage.jsx
+++ frontend/src/pages/LikePage.jsx
@@ -4,19 +4,20 @@ import LikedProfile from "../components/LikedProfile";
 
 function LikePage(prop){
 
+const [liked,setLiked] = useState([null]);
 const [likes,setLikes] = useState([null]);
 const [imgs,setImgs] = useState([null]);
 
 async function getLikes(){
-  if(likes[0] == null){
-   await axios.get("http://localhost:3004/updateLiked", {params:{username: prop.username}}).then(data => {setLikes(data.data.liked)} ).catch(err=>console.log(err))
+  if(liked[0] == null){
+   await axios.get("http://localhost:3004/updateLiked", {params:{username: prop.username}}).then(data => {setLiked(data.data.liked)} ).catch(err=>console.log(err))
   
     }
 }
 getLikes();
     return <div id="LikePage">
        <div id="Liked">
-        {likes.map((e,i)=> <LikedProfile key={i} username={e}></LikedProfile>)}
+        {liked.map((e,i)=> <LikedProfile key={i} username={e}></LikedProfile>)}
        </div>
       
       <div id="LikedBy"></div>
