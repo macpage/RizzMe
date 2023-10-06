c50fc14ac139332311db2f9a0346fe199cb9fa4a
Mac <sophanpa@gmail.com>
2023-10-06 21:05:41 +0200

    Edit

--- frontend/src/pages/LikePage.jsx
+++ frontend/src/pages/LikePage.jsx
@@ -8,16 +8,42 @@ const [liked,setLiked] = useState([null]);
 const [likes,setLikes] = useState([null]);
 const [imgs,setImgs] = useState([null]);
 
+const [showLikes,setShowLikes] = useState();
+
+
 async function getLikes(){
   if(liked[0] == null){
    await axios.get("http://localhost:3004/updateLiked", {params:{username: prop.username}}).then(data => {setLiked(data.data.liked)} ).catch(err=>console.log(err))
-  
+   
     }
 }
 getLikes();
+
+window.addEventListener("DOMContentLoaded", ()=>{
+  likeHandler();
+})
+
+function likeHandler(){
+  const showLikedBy = document.querySelector("#LikedBy");
+const showLiked = document.querySelector("#Liked");
+
+  console.log(showLikedBy)
+  console.log(showLiked);
+if(showLikes){
+  showLikedBy.style.display = "grid";
+  showLiked.style.display = "none";
+  console.log("its true");
+}else{
+  showLikedBy.style.display = "none";
+  showLiked.style.display = "grid";
+  console.log("its false");
+}
+
+
+}
     return <div id="LikePage">
-      <button>Likes you Got</button>
-      <button>Users you Liked</button>
+      <button onClick={() => setShowLikes(false)}>Likes you Got</button>
+      <button onClick={() => setShowLikes(true)}>Users you Liked</button>
        <div id="Liked">
 
         {liked.map((e,i)=> <LikedProfile key={i} username={e}></LikedProfile>)}
