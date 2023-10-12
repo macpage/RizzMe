4ffbbb21f0e644548504ea6cd4a6b9d72e027db7
Mac <sophanpa@gmail.com>
2023-10-13 01:51:16 +0200

    Edit

--- frontend/src/pages/LikePage.jsx
+++ frontend/src/pages/LikePage.jsx
@@ -2,6 +2,9 @@ import axios from "axios";
 import { useState } from "react";
 import LikedProfile from "../components/LikedProfile";
 
+
+
+
 function LikePage(prop){
 
 const [liked,setLiked] = useState([null]);
@@ -10,7 +13,9 @@ const [imgs,setImgs] = useState([null]);
 
 const [showLikes,setShowLikes] = useState();
 
-
+let isloaded = false;
+let showLikedBy;
+let showLiked ; 
 async function getLikes(){
   if(liked[0] == null){
    await axios.get("http://localhost:3004/updateLiked", {params:{username: prop.username}}).then(data => {setLiked(data.data.liked)} ).catch(err=>console.log(err))
@@ -18,14 +23,22 @@ async function getLikes(){
     }
 }
 getLikes();
+console.log(isloaded)
 
 window.addEventListener("DOMContentLoaded", ()=>{
   likeHandler();
+  
 })
 
+if(isloaded){
+  console.log("heyo")
+  console.log(showLiked);
+}
+
 function likeHandler(){
-  const showLikedBy = document.querySelector("#LikedBy");
-const showLiked = document.querySelector("#Liked");
+  isloaded = true;
+   showLikedBy = document.querySelector("#LikedBy");
+showLiked = document.querySelector("#Liked");
 
   console.log(showLikedBy)
   console.log(showLiked);
@@ -41,9 +54,24 @@ if(showLikes){
 
 
 }
+
+function change(show){
+  console.log("changed");
+  console.log(show);
+  showLikedBy = document.querySelector("#LikedBy");
+  showLiked = document.querySelector("#Liked");
+  if(!show){
+    showLikedBy.style.display = "grid";
+    showLiked.style.display = "none";
+  }else{
+    showLikedBy.style.display = "none";
+    showLiked.style.display = "grid";
+  }
+
+}
     return <div id="LikePage">
-      <button onClick={() => setShowLikes(false)}>Likes you Got</button>
-      <button onClick={() => setShowLikes(true)}>Users you Liked</button>
+      <button onClick={() =>  change(false)}>Likes you Got</button>
+      <button onClick={() => change(true)}>Users you Liked</button>
        <div id="Liked">
 
         {liked.map((e,i)=> <LikedProfile key={i} username={e}></LikedProfile>)}
